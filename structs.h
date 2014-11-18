80b51360bb4054872659ecaf12405d044a50d75e
aspirationunknown <aspirationunknown@gmail.com>
2014-11-18 12:01:54 -0700

    Added Body and Point structs with member variables.

---
+++ structs.h
@@ -0,0 +1,17 @@
+
+struct Point
+{
+    int x;
+    int y;
+    int z;
+}
+
+struct Body
+{
+    Point center;
+    float color[3];
+    double orbital_period;
+    double orbital_radius;
+    double radius;
+    double rotation;
+}
