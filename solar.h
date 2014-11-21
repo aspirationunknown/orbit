906e22eeaabde7072d454edfce15c78e9d6fd211
Porkenstein <portenstein@gmail.com>
2014-11-21 13:28:44 -0700

    initial commit of solar.h

---
+++ solar.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include "structs.h"
+#include <GL/freeglut.h>
+
+//keys
+const int EscapeKey = 27;
+
+//screens
+enum mode {SMOOTH, FLAT, WIREFRAME, TEXTURE};
+
+//function prototypes
